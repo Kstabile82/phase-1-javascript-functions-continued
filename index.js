c370cb55d2e3f34f7b8f8387c84242f86bc68d03
KarinaStabile <karina.stabile@gmail.com>
2021-08-10 22:25:12 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function wrapAdjective(str="*") {
+    return function (param ='special') {
+        return `You are ${str}${param}${str}!`;
+
+    }
+}
+function saturdayFun(arg1 = 'roller-skate') {
+    return `This Saturday, I want to ${arg1}!`; 
+}
+
+const mondayWork = function(param2 = 'go to the office') {
+    return `This Monday, I will ${param2}.`
+}
